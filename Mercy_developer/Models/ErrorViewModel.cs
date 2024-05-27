eb27a6d8bb19233aeb817416437845098bce88b5
ariel villalobos <ariel.villalobos.garcia@cftmail.cl>
2024-05-26 23:32:02 -0400

    Excluir .vs del control de versiones

--- Mercy_developer/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Mercy_developer.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
